e6232b04a1570006a56a5bdbf1dabe0756c66c50
Dan Tulovsky <dant@wetsnow.com>
2014-09-29 07:25:50 -0400

    Updates.

--- runworld.go
+++ runworld.go
@@ -1,29 +1,64 @@
 package main
 
 import (
-	"bufio"
+	"fmt"
 	"local/world"
 	"os"
 	"time"
+
+	termbox "github.com/nsf/termbox-go"
 )
 
 func main() {
-	s := &world.Settings{
-		NewPeep:         1,
-		MaxAge:          999,
-		RandomDeath:     0.0001,
-		NewPeepModifier: 1000,
+	// Initialize GUI
+	err := termbox.Init()
+	if err != nil {
+		panic(err)
 	}
-	w := world.NewWorld("Alpha1", *s)
-	go w.Run()
+	defer termbox.Close()
+
+	// Listen for input events on keyboard
+	event_queue := make(chan termbox.Event)
+	go func() {
+		for {
+			event_queue <- termbox.PollEvent()
+		}
+	}()
+
+	width, length := termbox.Size()
+	width = width / 2
+	length = length / 2
+	var height int32
+
+	s := &world.Settings{
+		NewPeep:          1,      // Initial chance of a new peep being spawned at origin
+		MaxAge:           2000,   // Any peep reaching this age will die
+		MaxPeeps:         500,    // Absolute max peeps in the world, no more can be born after this.
+		RandomDeath:      0.0001, // Chances of random death each turn for every peep
+		NewPeepMax:       500,    // Once this many peeps exist, no new ones are spawned from origin
+		NewPeepModifier:  1000,   // Controls how often new peeps spawn.  Lower is less often
+		Size:             &world.Size{int32(width), int32(length), height, int32(-width), int32(-length), -height},
+		SpawnProbability: 0.5, // Chances of two meetings peeps spawning a new one
+		TurnTime:         time.Millisecond * 50,
+	}
+	fmt.Fprintln(os.Stderr, s.Size)
+
+	w := world.NewWorld("Alpha1", *s, event_queue)
+	// go w.Run()
 
 	// Advance world every time user hits enter
-	scanner := bufio.NewScanner(os.Stdin)
+	// scanner := bufio.NewScanner(os.Stdin)
+	w.Show()
 
 	for {
-		scanner.Scan()
-		w.NextTurn()
-		w.Show()
-		time.Sleep(time.Microsecond * 3000)
+		// scanner.Scan()
+		if err := w.NextTurn(); err == nil {
+			w.Show()
+			time.Sleep(s.TurnTime)
+		} else {
+			fmt.Println(err)
+			os.Exit(0)
+		}
+
 	}
 }
